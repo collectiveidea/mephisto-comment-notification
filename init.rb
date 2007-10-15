29078d4b5a8418e1e303da9ceed2a2818a4fde0f
Luke Redpath <contact@lukeredpath.co.uk>
2007-10-15 14:07:25 +0000

    Updated readme and about.yml with new repository details

--- init.rb
+++ init.rb
@@ -1,8 +1,3 @@
 require 'mephisto_comment_notification'
 
-# this doesn't seem to be working for some reason
-# so lets just send the observer to actioncontroller
-#
-#config.active_record.observers = :comment_notifier_observer
-
 ActionController::Base.send(:observer, :comment_notifier_observer)
--- tasks/mephisto_comment_notification_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :mephisto_comment_notification do
-#   # Task goes here
-# end

c24500a2475be397346ae2fdd6803b7f976b57e1
Brandon Keepers <brandon@collectiveidea.com>
2008-11-10 23:22:18 -0500

    fix view path

--- init.rb
+++ init.rb
@@ -2,4 +2,4 @@ require 'comment_notifier'
 require 'comment_notifier_observer'
 
 ActiveRecord::Base.observers << :comment_notifier_observer
-ActionController::Base.append_view_path(File.dirname(__FILE__) + '/views')
+ActionController::Base.append_view_path(File.dirname(__FILE__) + '/lib/views')

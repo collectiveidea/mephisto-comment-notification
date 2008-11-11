38a208e6e57e9f9e1c2082301a1d3e18025934fa
Brandon Keepers <brandon@collectiveidea.com>
2008-11-10 22:00:15 -0500

    Use site of comment's article to find admin email

--- lib/comment_notifier_observer.rb
+++ lib/comment_notifier_observer.rb
@@ -1,5 +1,5 @@
 class CommentNotifierObserver < ActiveRecord::Observer
-  observe Comment
+  observe :comment
   
   def after_create(comment)
     send_comment_notification(comment) if comment.approved?
@@ -7,7 +7,7 @@ class CommentNotifierObserver < ActiveRecord::Observer
   
   protected
     def send_comment_notification(comment)
-      admin_email = Site.find(:first).email
+      admin_email = comment.article.site.email
       CommentNotifier.deliver_comment_notification(admin_email, comment)
     end
 end
